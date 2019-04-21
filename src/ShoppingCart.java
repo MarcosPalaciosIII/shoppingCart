7c9c4827c1e4faa03284bea7995e5637416d0831
Marcos <mp2772@gmail.com>
2019-04-21 19:27:12 -0400

    updated some spacing in shoppingCart file

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -2,6 +2,7 @@ public class ShoppingCart {
 
     public String item;
     public double price;
+
     ShoppingCart(String item, double price) {
         this.item = item;
         this.price = price;
